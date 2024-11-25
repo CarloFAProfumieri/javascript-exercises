9f9f15821fefa7d199f3deadbe79756eddfb2496
Carlo <profu95@gmail.com>
2024-11-25 00:50:36 -0300

    did excercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray2 = function(anArray, anItem = "") {
+    let length = anArray.length;
+    for (let i = 0; i < length; i++) {
+        if (anArray[i] === anItem) {
+            anArray.splice(i, 1);
+            i--;
+        }
+    }
+    return anArray
 };
-
+const removeFromArray = function(anArray, anItem, aSecondItem = "", aThirdItem = "", aFourthItem = "") {
+    anArray = removeFromArray2(anArray, anItem);
+    anArray = removeFromArray2(anArray, aSecondItem);
+    anArray = removeFromArray2(anArray, aThirdItem);
+    anArray = removeFromArray2(anArray, aFourthItem);
+    return anArray;
+};
 // Do not edit below this line
 module.exports = removeFromArray;
