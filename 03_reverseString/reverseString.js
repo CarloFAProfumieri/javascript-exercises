3a766d9b8c2fec95bcc9a2f9549f67d23efbb853
Carlo <profu95@gmail.com>
2024-11-25 00:27:33 -0300

    did excercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(aString) {
+    let length = aString.length;
+    let reversedString = ''
+    while (length >= 0) {
+        reversedString = reversedString.concat('',aString.charAt(length));
+        length--;
+    }
+    return reversedString;
 };
-
 // Do not edit below this line
 module.exports = reverseString;
