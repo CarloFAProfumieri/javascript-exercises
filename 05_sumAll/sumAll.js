5dbc82c1f2dd3a23d76208a9fd2fef202443b734
Carlo <profu95@gmail.com>
2024-11-25 10:43:15 -0300

    Summed them all (did excercise 5)

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+const sumAll = function(startingNumber, endingNumber) {
+    let sum = 0;
+    let swapAux = 0;
 
+    if (endingNumber < 0 || startingNumber < 0 || notInteger(startingNumber) || notInteger(endingNumber)) {
+        return "ERROR"
+    }
+
+    if(endingNumber < startingNumber){
+        swapAux = startingNumber;
+        startingNumber = endingNumber;
+        endingNumber = swapAux;
+    }
+
+    for (let i = startingNumber; i <= endingNumber; i++) {
+        sum += i;
+    }
+    return sum;
 };
+
+function notInteger(aNumber){
+    let aNumberRounded = Math.floor(aNumber);
+    return !(aNumberRounded === aNumber);
+}
 
 // Do not edit below this line
 module.exports = sumAll;
