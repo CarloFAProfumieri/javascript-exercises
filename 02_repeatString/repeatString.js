6bde0764494fc236665e1da2bdcce9a199e55c89
Carlo <profu95@gmail.com>
2024-11-24 22:10:00 -0300

    Did excercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(aString, iterations) {
+    let timesRepeated = 0;
+    let finalString = '';
 
+    if (iterations < 0) {
+        return "ERROR";
+    }
+
+    while (timesRepeated < iterations) {
+        finalString += aString;
+        timesRepeated++;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
